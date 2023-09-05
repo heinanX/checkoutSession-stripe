4c15366d2850bc308d6ffe515ac7fc1412e66cba
heinanX <larrii@live.se>
2023-09-05 15:30:49 +0200

    server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+
+app.listen(3000, () => { console.log('Server is up and running..') })
