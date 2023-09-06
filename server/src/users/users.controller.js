dd40c5521ac4ba4340374c813e673a9c01059ac9
heinanX <larrii@live.se>
2023-09-06 20:05:11 +0200

    fs works

--- server/src/users/users.controller.js
+++ server/src/users/users.controller.js
@@ -14,13 +14,30 @@ const getUsers = async (req, res) => {
 }
 
 const createUser = async (req, res) => {
-  try {
-   
+  try { 
   const customer = await stripe.customers.create({
     email: req.body.email,
     description: req.body.description
   });
-  res.status(200).json({ customer })
+  res.status(201).json({ customer })
+
+  const { email, description} = req.body
+
+    fs.readFile('./src/db/users.json', (err, data) => {
+      if (err) { res.status(404).send(`Unable to read source file. See ${err}`) }
+      const userData = JSON.parse(data);
+    
+      userData.push(
+        {
+          email: email,
+          password: req.hashedPassword
+        }
+      );
+  
+      fs.writeFile('./src/db/users.json', JSON.stringify(userData, null, 2), function (err) {
+        if (err) { res.status(404).send(`Unable to write file. See ${err}`) }});
+    })
+   
   } catch (error) {
     console.error(error);
   }
